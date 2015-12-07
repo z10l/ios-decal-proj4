d4248aa0bd197fdc7193765a94ae617eb821431f
crls1 <carloscaballero@berkeley.edu>
2015-12-06 17:18:15 -0800

    added model

---
+++ iCard/iCard/Category.swift
@@ -0,0 +1,23 @@
+//
+//  Category.swift
+//  iCard
+//
+//  Created by Carlos Caballero on 12/6/15.
+//  Copyright © 2015 iosdecal. All rights reserved.
+//
+
+import UIKit
+
+class Data: NSObject {
+    var categories = [[String]]()
+    
+    func getCategories() {
+        
+    }
+    
+    func getQuestionsByCategory(id: Int) ->[[String]] {
+        return [[String]]()
+    }
+}
+
+
